bd74b7179b28ff06c9090bcde5612a7f404c866f
Mafessita <maria.garciaco@cecar.edu.co>
2024-10-14 23:01:32 -0500

    Metiene mal con ese CU L O

---
+++ metienemal/metienemal/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace metienemal
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ metienemal/metienemal/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace metienemal
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
