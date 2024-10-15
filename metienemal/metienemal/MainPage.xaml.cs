96d64ee3b9e03d0e5f539fd3dbea2f29fe2cb4d8
Mafessita <maria.garciaco@cecar.edu.co>
2024-10-15 00:48:14 -0500

    Boton nuevo

--- metienemal/metienemal/MainPage.xaml.cs
+++ metienemal/metienemal/MainPage.xaml.cs
@@ -2,26 +2,15 @@
 {
     public partial class MainPage : ContentPage
     {
-<<<<<<< HEAD
-
-=======
-<<<<<<< Updated upstream
-        
-=======
-  
->>>>>>> Stashed changes
->>>>>>> 068d7bda86985f9925b654dbe648f0a3cbe003f4
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-
         private void OnButtonClicked(object sender, EventArgs e)
         {
             DisplayAlert("Button Pressed", "You pressed the button!", "OK");
-         }
+        }
         private void OnNewButtonClicked(object sender, EventArgs e)
         {
             DisplayAlert("New Button", "This button was added by a collaborator!", "OK");
@@ -31,13 +20,13 @@
             var uri = new Uri("https://example.com");
             await Launcher.Default.OpenAsync(uri);
         }
-
         private async void OnNavigateButtonClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new pagina1());
+            await Navigation.PushAsync(new Pagina1());
         }
         private void OnChangeColorButtonClicked(object sender, EventArgs e)
         {
             this.BackgroundColor = Colors.LightBlue;
         }
     }
+}
--- metienemal/metienemal/Pagina1.xaml.cs
+++ metienemal/metienemal/Pagina1.xaml.cs
@@ -1,14 +1,8 @@
 namespace metienemal;
 
-<<<<<<< HEAD
 public partial class Pagina1 : ContentPage
 {
 	public Pagina1()
-=======
-public partial class Page1 : ContentPage
-{
-	public Page1()
->>>>>>> codigo/alex
 	{
 		InitializeComponent();
 	}
