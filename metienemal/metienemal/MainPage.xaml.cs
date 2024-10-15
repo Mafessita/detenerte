a4e9b031c1cdaf1ffc58897b2a7b0ffed482abe3
AlexG0311 <alexdavidgonzalez2003@gmail.com>
2024-10-14 23:57:54 -0500

    app

--- metienemal/metienemal/MainPage.xaml.cs
+++ metienemal/metienemal/MainPage.xaml.cs
@@ -9,25 +9,17 @@
             InitializeComponent();
         }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> deb30bc980d41652500a3fb61616676462d5faf6
+
         private void OnButtonClicked(object sender, EventArgs e)
         {
             DisplayAlert("Button Pressed", "You pressed the button!", "OK");
-=======
+         }
         private void OnNewButtonClicked(object sender, EventArgs e)
         {
             DisplayAlert("New Button", "This button was added by a collaborator!", "OK");
->>>>>>> Aporte/maria
-        }
-<<<<<<< HEAD
->>>>>>> 8b26a786951c12daaae54bcd3ef954773af223f1
-=======
 
->>>>>>> deb30bc980d41652500a3fb61616676462d5faf6
+        }
+
     }
 
 }
